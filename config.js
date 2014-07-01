5c25f78c18ed43c388d3e2a1a060c33a9af249e2
HarleyKwyn <kwyn.meagher@gmail.com>
2014-07-01 12:25:54 -0700

    Adds caching functionality using catbox-redis

--- config.js
+++ config.js
@@ -1,20 +1,10 @@
-var Joi = require('Joi');
-
-exports.helloConfig = {
-	handler : function(req, res){
-		var name = req.params.name.split("/");
-      res({
-      	first : name[0],
-      	last : name[1],
-      	mood : req.query.mood || "neutral"
-      });
-	},
-	validate : {
-		params: {
-			name: Joi.string().min(8).max(100)
-		},
-		query:  {
-			mood: Joi.string().valid(["neutral","happy","sad"]).default("neutral")
-		}
-	}
-}
+exports.server  = {
+	cache: {
+    engine: "catbox-redis",
+    options: {
+      host: "localhost",
+      partition: "MyApp",
+      password: "mypassword"
+    }
+  }
+};
--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 var Hapi = require('Hapi');
-var config = require('./config.js')
+var Joi = require('Joi');
+var config = require('./config.js');
 
-var server = new Hapi.Server(8080, "localhost");
+var server = new Hapi.Server(8080, "localhost", config.server);
 server.start(function() {
     console.log("Hapi server started @", server.info.uri);
 });
 
+server.method("getColour", function(name, next){
+	var colours = ["red", "blue", "indigo", "violet", "green"];
+  var colour = colours[Math.floor(Math.random() * colours.length)];
+  next(null, colour);
+},{
+	cache : {
+		expiresIn: 30000
+	}
+});
 
+var helloConfig = {
+	handler : function(req, res){
+		var name = req.params.name.split("/");
+			server.methods.getColour(req.params.name, function(err, colour){
+	      res({
+	      	first : name[0],
+	      	last : name[1],
+	      	mood : req.query.mood || "neutral",
+	      	colour : colour
+	      });	
+			})
+	},
+	validate : {
+		params: {
+			name: Joi.string().min(8).max(100)
+		},
+		query:  {
+			mood: Joi.string().valid(["neutral","happy","sad"]).default("neutral")
+		}
+	}
+};
 
 server.route({
     path: "/hello/{name*2}",
     method: "GET",
-    config: config.helloConfig
+    config: helloConfig
 });
